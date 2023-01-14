312f03aca24f2987292523497dd35248411ec926
panchalnimesh <npanchal@sph.com.sg>
2023-01-15 07:23:44 +0800

    create rds

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -8,6 +8,7 @@ module "vpc" {
   azs             = ["ap-southeast-1a", "ap-southeast-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
 
   enable_nat_gateway = true
 
