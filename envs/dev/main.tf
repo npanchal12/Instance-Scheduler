7426d9fbc93f4614d329d12ae10958d5cb1afd51
panchalnimesh <npanchal@sph.com.sg>
2023-01-15 07:37:45 +0800

    created resources

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,156 +1,156 @@
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "~>3.19.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~>3.19.0"
 
-#   name = "my-dev-vpc"
-#   cidr = "10.0.0.0/16"
+  name = "my-dev-vpc"
+  cidr = "10.0.0.0/16"
 
-#   azs             = ["ap-southeast-1a", "ap-southeast-1b"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-#   database_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
 
-#   enable_nat_gateway = true
+  enable_nat_gateway = true
 
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 
-# ################################################################################
-# # RDS Aurora Module
-# ################################################################################
+################################################################################
+# RDS Aurora Module
+################################################################################
 
-# module "rds-aurora" {
-#   source  = "terraform-aws-modules/rds-aurora/aws"
-#   version = "7.6.0"
+module "rds-aurora" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "7.6.0"
 
-#   name           = local.name
-#   engine         = "aurora-mysql"
-#   engine_version = "5.7"
-#   instances = {
-#     1 = {
-#       instance_class      = "db.r5.large"
-#       publicly_accessible = true
-#     }
-#     2 = {
-#       identifier     = "mysql-static-1"
-#       instance_class = "db.r5.2xlarge"
-#     }
-#     3 = {
-#       identifier     = "mysql-excluded-1"
-#       instance_class = "db.r5.xlarge"
-#       promotion_tier = 15
-#     }
-#   }
+  name           = local.name
+  engine         = "aurora-mysql"
+  engine_version = "5.7"
+  instances = {
+    1 = {
+      instance_class      = "db.r5.large"
+      publicly_accessible = true
+    }
+    2 = {
+      identifier     = "mysql-static-1"
+      instance_class = "db.r5.2xlarge"
+    }
+    3 = {
+      identifier     = "mysql-excluded-1"
+      instance_class = "db.r5.xlarge"
+      promotion_tier = 15
+    }
+  }
 
-#   vpc_id                 = module.vpc.vpc_id
-#   db_subnet_group_name   = module.vpc.database_subnet_group_name
-#   create_db_subnet_group = true
-#   create_security_group  = true
-#   allowed_cidr_blocks    = module.vpc.private_subnets_cidr_blocks
+  vpc_id                 = module.vpc.vpc_id
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
+  create_db_subnet_group = true
+  create_security_group  = true
+  allowed_cidr_blocks    = module.vpc.private_subnets_cidr_blocks
 
-#   iam_database_authentication_enabled = true
-#   master_password                     = random_password.master.result
-#   create_random_password              = true
+  iam_database_authentication_enabled = true
+  master_password                     = random_password.master.result
+  create_random_password              = true
 
-#   apply_immediately   = true
-#   skip_final_snapshot = true
+  apply_immediately   = true
+  skip_final_snapshot = true
 
-#   create_db_cluster_parameter_group      = true
-#   db_cluster_parameter_group_name        = local.name
-#   db_cluster_parameter_group_family      = "aurora-mysql5.7"
-#   db_cluster_parameter_group_description = "${local.name} example cluster parameter group"
-#   db_cluster_parameter_group_parameters = [
-#     {
-#       name         = "connect_timeout"
-#       value        = 120
-#       apply_method = "immediate"
-#       }, {
-#       name         = "innodb_lock_wait_timeout"
-#       value        = 300
-#       apply_method = "immediate"
-#       }, {
-#       name         = "log_output"
-#       value        = "FILE"
-#       apply_method = "immediate"
-#       }, {
-#       name         = "max_allowed_packet"
-#       value        = "67108864"
-#       apply_method = "immediate"
-#       }, {
-#       name         = "aurora_parallel_query"
-#       value        = "OFF"
-#       apply_method = "pending-reboot"
-#       }, {
-#       name         = "binlog_format"
-#       value        = "ROW"
-#       apply_method = "pending-reboot"
-#       }, {
-#       name         = "log_bin_trust_function_creators"
-#       value        = 1
-#       apply_method = "immediate"
-#       }, {
-#       name         = "require_secure_transport"
-#       value        = "ON"
-#       apply_method = "immediate"
-#       }, {
-#       name         = "tls_version"
-#       value        = "TLSv1.2"
-#       apply_method = "pending-reboot"
-#     }
-#   ]
+  create_db_cluster_parameter_group      = true
+  db_cluster_parameter_group_name        = local.name
+  db_cluster_parameter_group_family      = "aurora-mysql5.7"
+  db_cluster_parameter_group_description = "${local.name} example cluster parameter group"
+  db_cluster_parameter_group_parameters = [
+    {
+      name         = "connect_timeout"
+      value        = 120
+      apply_method = "immediate"
+      }, {
+      name         = "innodb_lock_wait_timeout"
+      value        = 300
+      apply_method = "immediate"
+      }, {
+      name         = "log_output"
+      value        = "FILE"
+      apply_method = "immediate"
+      }, {
+      name         = "max_allowed_packet"
+      value        = "67108864"
+      apply_method = "immediate"
+      }, {
+      name         = "aurora_parallel_query"
+      value        = "OFF"
+      apply_method = "pending-reboot"
+      }, {
+      name         = "binlog_format"
+      value        = "ROW"
+      apply_method = "pending-reboot"
+      }, {
+      name         = "log_bin_trust_function_creators"
+      value        = 1
+      apply_method = "immediate"
+      }, {
+      name         = "require_secure_transport"
+      value        = "ON"
+      apply_method = "immediate"
+      }, {
+      name         = "tls_version"
+      value        = "TLSv1.2"
+      apply_method = "pending-reboot"
+    }
+  ]
 
-#   create_db_parameter_group      = true
-#   db_parameter_group_name        = local.name
-#   db_parameter_group_family      = "aurora-mysql5.7"
-#   db_parameter_group_description = "${local.name} example DB parameter group"
-#   db_parameter_group_parameters = [
-#     {
-#       name         = "connect_timeout"
-#       value        = 60
-#       apply_method = "immediate"
-#       }, {
-#       name         = "general_log"
-#       value        = 0
-#       apply_method = "immediate"
-#       }, {
-#       name         = "innodb_lock_wait_timeout"
-#       value        = 300
-#       apply_method = "immediate"
-#       }, {
-#       name         = "log_output"
-#       value        = "FILE"
-#       apply_method = "pending-reboot"
-#       }, {
-#       name         = "long_query_time"
-#       value        = 5
-#       apply_method = "immediate"
-#       }, {
-#       name         = "max_connections"
-#       value        = 2000
-#       apply_method = "immediate"
-#       }, {
-#       name         = "slow_query_log"
-#       value        = 1
-#       apply_method = "immediate"
-#       }, {
-#       name         = "log_bin_trust_function_creators"
-#       value        = 1
-#       apply_method = "immediate"
-#     }
-#   ]
+  create_db_parameter_group      = true
+  db_parameter_group_name        = local.name
+  db_parameter_group_family      = "aurora-mysql5.7"
+  db_parameter_group_description = "${local.name} example DB parameter group"
+  db_parameter_group_parameters = [
+    {
+      name         = "connect_timeout"
+      value        = 60
+      apply_method = "immediate"
+      }, {
+      name         = "general_log"
+      value        = 0
+      apply_method = "immediate"
+      }, {
+      name         = "innodb_lock_wait_timeout"
+      value        = 300
+      apply_method = "immediate"
+      }, {
+      name         = "log_output"
+      value        = "FILE"
+      apply_method = "pending-reboot"
+      }, {
+      name         = "long_query_time"
+      value        = 5
+      apply_method = "immediate"
+      }, {
+      name         = "max_connections"
+      value        = 2000
+      apply_method = "immediate"
+      }, {
+      name         = "slow_query_log"
+      value        = 1
+      apply_method = "immediate"
+      }, {
+      name         = "log_bin_trust_function_creators"
+      value        = 1
+      apply_method = "immediate"
+    }
+  ]
 
-#   enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
-#   security_group_use_name_prefix  = false
+  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  security_group_use_name_prefix  = false
 
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#     Name        = "sql-aurora-dev"
-#   }
-# }
-# resource "random_password" "master" {
-#   length = 10
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Name        = "sql-aurora-dev"
+  }
+}
+resource "random_password" "master" {
+  length = 10
+}
