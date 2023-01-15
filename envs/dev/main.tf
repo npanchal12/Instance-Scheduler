d8e4783012ea89ad44aefbda88e5999de50ea0d3
panchalnimesh <npanchal@sph.com.sg>
2023-01-15 11:17:36 +0800

    update sg details

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -161,7 +161,7 @@ module "ec2_jumphost" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t3.medium"
   subnet_id                   = module.vpc.private_subnets[0]
-  vpc_security_group_ids      = module.vote_service_sg.security_group_id
+  vpc_security_group_ids      = ["sg-0c749d439a29fe9fc"]
   associate_public_ip_address = false
   create_iam_instance_profile = true
   iam_role_description        = "IAM role for EC2 instance"
