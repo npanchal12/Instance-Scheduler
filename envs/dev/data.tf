155bb274eebfa281c2f1c69f41fd03d626b193d1
panchalnimesh <npanchal@sph.com.sg>
2023-02-17 15:04:00 +0800

    update policy

--- envs/dev/data.tf
+++ envs/dev/data.tf
@@ -2,6 +2,7 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
+
 data "aws_iam_policy_document" "instance_scheduler_policy" {
 
   statement {
@@ -25,6 +26,25 @@ data "aws_iam_policy_document" "instance_scheduler_policy" {
       "*"
     ]
   }
+
+  statement {
+    sid = "invokelambdafunction"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    effect = "Allow"
+    resources = [
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_stop_ec2.lambda_function_name}*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_stop_ec2.lambda_function_name}:*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_start_ec2.lambda_function_name}*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_start_ec2.lambda_function_name}:*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_stop_rds.lambda_function_name}*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_stop_rds.lambda_function_name}:*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_start_rds.lambda_function_name}*",
+      "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_start_rds.lambda_function_name}:*"
+    ]
+  }
+}
   # statement {
   #   sid = "invokelambdafunction"
   #   actions = [
@@ -38,7 +58,7 @@ data "aws_iam_policy_document" "instance_scheduler_policy" {
   #     "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${module.lambda_function_start_ec2.lambda_function_name}:*"
   #   ]
   # }
-}
+# }
 
 data "aws_iam_policy_document" "eventbridge_schedule_policy" {
 
