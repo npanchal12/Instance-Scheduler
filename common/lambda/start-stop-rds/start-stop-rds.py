d4988c6a8334b798e4e62b651886ff00f4ce3550
panchalnimesh <npanchal@sph.com.sg>
2023-02-24 13:37:15 +0800

    update python code

--- common/lambda/start-stop-rds/start-stop-rds.py
+++ common/lambda/start-stop-rds/start-stop-rds.py
@@ -1,23 +1,21 @@
 import boto3
-import json
 
 def lambda_handler(event, context):
-    # parse the input payload
-    payload = json.loads(event['body'])
-    desired_status = payload['status']
-    
-    # create an RDS client
+    # Extract the desired status from the input event
+    desired_status = event['status']
+
+    # Create an RDS client
     rds_client = boto3.client('rds')
 
-    # get information about all RDS clusters
+    # Get information about all RDS clusters
     response = rds_client.describe_db_clusters()
 
-    # iterate through the clusters and check their status
+    # Iterate through the clusters and check their status
     for cluster in response['DBClusters']:
         cluster_name = cluster['DBClusterIdentifier']
         cluster_status = cluster['Status']
 
-        # if the cluster is in the desired status, stop or start it
+        # If the cluster is in the desired status, stop or start it
         if cluster_status == desired_status:
             response = rds_client.stop_db_cluster(
                 DBClusterIdentifier=cluster_name
@@ -30,8 +28,8 @@ def lambda_handler(event, context):
             print(f"Started cluster {cluster_name}")
         else:
             print(f"Skipping cluster {cluster_name} with status {cluster_status}")
-    
-    # return a success message
+
+    # Return a success message
     return {
         'statusCode': 200,
         'body': f"All RDS clusters in {desired_status} status have been stopped or started."
