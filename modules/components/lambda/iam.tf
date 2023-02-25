e39579862691494f39e5ccdbcd1e548d3fac6af0
panchalnimesh <npanchal@sph.com.sg>
2023-02-25 18:15:42 +0800

    remove unwated items

--- modules/components/lambda/iam.tf
+++ modules/components/lambda/iam.tf
@@ -1,23 +1,3 @@
-# resource "aws_iam_role_policy_attachment" "ec2_stop_role_attachment" {
-#   role       = module.lambda_function_stop_ec2.lambda_function_name
-#   policy_arn = aws_iam_policy.instance_scheduler_policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "ec2_start_role_attachment" {
-#   role       = module.lambda_function_start_ec2.lambda_function_name
-#   policy_arn = aws_iam_policy.instance_scheduler_policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "rds_stop_role_attachment" {
-#   role       = module.lambda_function_stop_rds.lambda_function_name
-#   policy_arn = aws_iam_policy.instance_scheduler_policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "rds_start_role_attachment" {
-#   role       = module.lambda_function_start_rds.lambda_function_name
-#   policy_arn = aws_iam_policy.instance_scheduler_policy.arn
-# }
-
 resource "aws_iam_role_policy_attachment" "rds_start_stop_role_attachment" {
   role       = module.lambda_function_start_stop_rds.lambda_function_name
   policy_arn = aws_iam_policy.instance_scheduler_policy.arn
--- modules/components/scheduler/data.tf
+++ modules/components/scheduler/data.tf
@@ -11,14 +11,6 @@ data "aws_iam_policy_document" "eventbridge_schedule_policy" {
     ]
     effect = "Allow"
     resources = [
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_stop_ec2_name}*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_stop_ec2_name}:*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_ec2_name}*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_ec2_name}:*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_stop_rds_name}*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_stop_rds_name}:*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_rds_name}*",
-      # "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_rds_name}:*"
       "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_stop_rds_name}*",
       "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_stop_rds_name}:*",
       "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:function:${var.lambda_function_start_stop_non_asg_ec2_name}*",
