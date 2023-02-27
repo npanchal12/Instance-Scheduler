8ffa25e20247cd1c6c896e3c01e22e16523a03d5
panchalnimesh <npanchal@sph.com.sg>
2023-02-28 07:26:04 +0800

    create resources again

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,16 +1,16 @@
 
-# module "scheduler" {
-#   source = "../../modules/components/scheduler"
+module "scheduler" {
+  source = "../../modules/components/scheduler"
 
-#   lambda_function_start_stop_rds_arn          = module.lambda_function.lambda_function_start_stop_rds_arn
-#   lambda_function_start_stop_rds_name         = module.lambda_function.lambda_function_start_stop_rds_name
-#   lambda_function_start_stop_non_asg_ec2_arn  = module.lambda_function.lambda_function_start_stop_non_asg_ec2_arn
-#   lambda_function_start_stop_non_asg_ec2_name = module.lambda_function.lambda_function_start_stop_non_asg_ec2_name
-# }
+  lambda_function_start_stop_rds_arn          = module.lambda_function.lambda_function_start_stop_rds_arn
+  lambda_function_start_stop_rds_name         = module.lambda_function.lambda_function_start_stop_rds_name
+  lambda_function_start_stop_non_asg_ec2_arn  = module.lambda_function.lambda_function_start_stop_non_asg_ec2_arn
+  lambda_function_start_stop_non_asg_ec2_name = module.lambda_function.lambda_function_start_stop_non_asg_ec2_name
+}
 
-# module "lambda_function" {
-#   source = "../../modules/components/lambda"
+module "lambda_function" {
+  source = "../../modules/components/lambda"
 
-#   start_stop_rds_code         = "${path.module}/../../common/build/start-stop-rds/start-stop-rds.zip"
-#   start_stop_non_asg_ec2_code = "${path.module}/../../common/build/start-stop-non-asg-ec2/start-stop-non-asg-ec2.zip"
-# }
+  start_stop_rds_code         = data.archive_file.start_stop_rds_zip_file.output_path
+  start_stop_non_asg_ec2_code = data.archive_file.start_stop_non-asg-ec2_zip_file.output_path
+}
